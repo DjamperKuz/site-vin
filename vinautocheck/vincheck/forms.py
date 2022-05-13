e55b2a2068cbd6fa334c53f3301c2b81adee441a
Igor Eremeev <werule2314@gmail.com>
2022-05-13 23:51:16 +0300

    Подготовка к корректной работе изменения данных в личном кабинете

--- vinautocheck/vincheck/forms.py
+++ vinautocheck/vincheck/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, SetPasswordForm
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
 # регистрация пользователя
@@ -36,15 +37,26 @@ class LoginUserForm(AuthenticationForm):
 
 
 class VINForm(forms.Form):
-    vin = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'your_vin', 'placeholder': 'Укажите VIN'}))
+    vin = forms.CharField(min_length=17, max_length=17, label='', widget=forms.TextInput(attrs={'class': 'your_vin', 'placeholder': 'Укажите VIN'}))
 
 
 class RecoveryPassForm(forms.Form):
     email = forms.CharField(label='', widget=forms.EmailInput(attrs={'class': 'txtb', 'placeholder': 'Почта'}))
 
 
-# class CreateNewPassword(SetPasswordForm):
-#     password1 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'class': 'txtb',
-#                                                                             'placeholder': 'Пароль'}))
-#     password2 = forms.CharField(label='', widget=forms.PasswordInput(attrs={'class': 'txtb',
-#                                                                             'placeholder': 'Подтвердите пароль'}))
+# Self cabinet try
+# class UserForm(forms.Form):
+#
+#     class Meta:
+#         # Set this form to use the User model.
+#         model = get_user_model
+#
+#         # Constrain the UserForm to just these fields.
+#         fields = ("first_name", "last_name", "password1", "password2")
+#
+#     def save(self, user):
+#         user.first_name = self.cleaned_data['first_name']
+#         user.last_name = self.cleaned_data['last_name']
+#         user.password1 = self.cleaned_data['password1']
+#         user.password2 = self.cleaned_data['password2']
+#         user.save()
